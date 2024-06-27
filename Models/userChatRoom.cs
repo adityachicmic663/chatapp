5ba368104f7edac491e5902987d2646f718e5c1f
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:35:39 +0530

    Update userChatRoom.cs

--- Models/userChatRoom.cs
+++ Models/userChatRoom.cs
@@ -1,6 +1,14 @@
-﻿namespace backendChatApplcation.Models
+using backendChatApplication.Models;
+
+namespace backendChatApplcation.Models
 {
     public class userChatRoom
     {
+        public int userId { get; set; }
+        public int chatRoomId { get; set; }
+        public DateTime joinedAt { get; set; } = DateTime.Now;
+
+        public UserModel User { get; set; }
+        public chatRoomModel ChatRoom { get; set; }
     }
 }
