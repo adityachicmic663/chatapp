73d96b0fdd49f8b01885ea3252d4f49363c01438
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:32:31 +0530

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+using backendChatApplcation.Services;
+using backendChatApplication.Models;
+using backendChatApplication.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backendChatApplcation.Controllers
@@ -7,131 +10,72 @@ namespace backendChatApplcation.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-     private readonly IchatServices _chatService;
- public ChatController(IchatServices chatService)
- {
-     _chatService = chatService;
- }
- [HttpGet("GetRoomForUsers/{userId}")]
- public ActionResult<IEnumerable<chatRoomModel>> GetRoomForUsers(int userId)
- {
-     try
-     {
-         var chatRooms = _chatService.GetChatRoomsForUser(userId);
-         return Ok(new ResponseModel
-         {
-             statusCode = 200,
-             message = "get all your chat rooms here",
-             data = chatRooms,
-             isSuccess = true
-         });
-     } catch (Exception ex)
-     {
-         return StatusCode(500, new ResponseModel
-         {
-             statusCode = 500,
-             message = ex.InnerException?.Message ?? ex.Message,
-             data = "No data",
-             isSuccess = false
-         });
-     }
- }
+        private readonly IUserServices _userService;
 
- [HttpPost("CreateChatRoom")]
- public ActionResult<chatRoomModel> CreateChatRoom(ChatRoomRequest request)
- {
-     try
-     {
-         var newRoom = _chatService.CreateChatRoom(request.RoomName, request.CreatorId);
-         if (newRoom == null){
-             return BadRequest(new ResponseModel
-             {
-                 statusCode = 404,
-                 message = "no room created",
-                 data = "no data",
-                 isSuccess = false
+        public UserController(IUserServices userService)
+        {
+            _userService = userService;
+        }
 
-             });
-         }
-         return Ok(new ResponseModel
-         {
-             statusCode=200,
-             message="room created successfully",
-             data= newRoom,
-             isSuccess=true
-         });
-     }catch(Exception ex)
-     {
-         return StatusCode(500, new ResponseModel
-         {
-             statusCode = 500,
-             message = ex.InnerException?.Message ?? ex.Message,
-             data = "No data",
-             isSuccess = false
-         });
-     }
-    
- }
- [HttpPost("sendMessage")]
- public ActionResult<chatMessage> SendMessage(sendMessageRequest request)
- {
-     try
-     {
-         var newMessage = _chatService.SendMessage(request.senderId, request.chatRoomId, request.message);
-         if (newMessage == null)
-         {
-             return BadRequest(new ResponseModel
-             {
-                 statusCode=400,
-                 message="unable to send message",
-                 data="no data",
-                 isSuccess=false
-
-             });
-         }
-         return Ok(new ResponseModel
-         {
-             statusCode = 200,
-             message = "message is sent successfully",
-             data = newMessage,
-             isSuccess = true
-         });
-     }catch(Exception ex)
-     {
-         return StatusCode(500, new ResponseModel
-         {
-             statusCode = 500,
-             message = ex.InnerException?.Message ?? ex.Message,
-             data = "no data",
-             isSuccess = true
-         });
-     }
- }
- [HttpPut("AddUsers")]
-
- public ActionResult AddUserToChatRoom(AddUsersRequest request)
- {
-     try
-     {
-         _chatService.AddUserToChatRoom(request.userId, request.chatRoomId)
-         return Ok(new ResponseModel
-         {
-             statusCode = 200,
-             message = "status updated successfully",
-             data = "non data",
-             isSuccess = true
-         });
-     }
-     catch (Exception ex)
-     {
-         return StatusCode(500, new ResponseModel
-         {
-             statusCode = 500,
-             message = ex.Message,
-             data = "no data",
-             isSuccess = false,
-         });
-     }
- }
+        [HttpGet("searchUser")]
+        public ActionResult<List<UserModel>> SearchUser(string searchKey)
+        {
+            try
+            {
+                var listofUsers = _userService.SearchUser(searchKey);
+                if (listofUsers == null)
+                {
+                    return NotFound(new ResponseModel
+                    {
+                        statusCode=404,
+                        message="user not found",
+                        data="no data",
+                        isSuccess=false
+                    });
+                }
+                return Ok(new ResponseModel
+                {
+                    statusCode = 200,
+                    message = "all the users are here",
+                    data=listofUsers,
+                    isSuccess=true
+                });
+            }catch(Exception ex)
+            {
+                return StatusCode(500, new ResponseModel
+                {
+                    statusCode=500,
+                    message=ex.Message,
+                    data="no data",
+                    isSuccess=false
+                });
+            }
+        }
+        [HttpPut("updateStatus")]
+        public ActionResult UpdateUserStatus(int userId,bool isOnline)
+        {
+            try
+            {
+                _userService.UpdateUserStatus(userId, isOnline);
+                return Ok(new ResponseModel
+                {
+                    statusCode=200,
+                    message="status updated successfully",
+                    data="non data",
+                    isSuccess=true
+                });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new ResponseModel
+                {
+                    statusCode=500,
+                    message=ex.Message,
+                    data="no data",
+                    isSuccess=false,
+                });
+            }
+        }
+       
     }
 }
