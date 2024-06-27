9f520416ee97a8d0ab0c59bd100c97fe88d6c634
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:33:16 +0530

    Update chatHub.cs

--- Hubs/chatHub.cs
+++ Hubs/chatHub.cs
@@ -1,6 +1,40 @@
-﻿namespace backendChatApplcation.Hubs
+using backendChatApplcation.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace backendChatApplcation.Hubs
 {
-    public class chatHub
+    public class chatHub:Hub
     {
+        private readonly chatServices _chatService;
+
+        public chatHub(chatServices chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task SendMessage(int senderId,int chatRoomId,string message)
+        {
+            _chatService.SendMessage(senderId, chatRoomId, message);
+
+            await Clients.Group(chatRoomId.ToString()).SendAsync("ReceivedMessage", senderId, message); 
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            string userId = Context.User.Identity.Name;  // assuming userId is stored in Claims
+            await Groups.AddToGroupAsync(Context.ConnectionId, userId);  //Add user to a group(chat room) based on userId
+
+            await Clients.Others.SendAsync("UserConnected", userId);  //Notify clients of user status change
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            string userId = Context.User.Identity.Name;
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, userId);// remove user from groups and notify other of disconnection
+            await Clients.Others.SendAsync("user disconnected", userId);
+
+            await base.OnDisconnectedAsync(ex);
+        }
+
     }
 }
