542e8e6d5db83f9e08f5e550b00922fea5517c17
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:35:09 +0530

    Update chatRoomModel.cs

--- Models/chatRoomModel.cs
+++ Models/chatRoomModel.cs
@@ -1,6 +1,19 @@
-﻿namespace backendChatApplcation.Models
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backendChatApplcation.Models
 {
-    public class ChatRoomModel
+    public class chatRoomModel
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int chatRoomId { get; set; }
+
+        public string chatRoomName { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<userChatRoom> UserChatRooms { get; set; } = new List<userChatRoom>();
+
+        public ICollection<chatMessage> Messages { get; set; } = new List<chatMessage>();
+
     }
 }
