19820095d4dcbb48b77e463768cc34e906e1976d
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:39:29 +0530

    Update chatServices.cs

--- Services/chatServices.cs
+++ Services/chatServices.cs
@@ -1,6 +1,70 @@
-﻿namespace backendChatApplcation.Services
+using backendChatApplcation.Models;
+using backendChatApplication;
+
+namespace backendChatApplcation.Services
 {
-    public class chatServices
+    public class chatServices:IchatServices
     {
+        private readonly chatDataContext _context;
+
+        public chatServices(chatDataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        public List<chatRoomModel> GetChatRoomsForUser(int userId)
+        {
+            return _context.UserChatRooms.Where(ucr => ucr.userId == userId).Select(ucr=>ucr.ChatRoom).ToList();
+        }
+
+        public chatRoomModel CreateChatRoom(string roomName, int creatorId)
+        {
+            var newRoom = new chatRoomModel
+            {
+                chatRoomName = roomName,
+                CreatedAt = DateTime.Now
+            };
+            _context.ChatRooms.Add(newRoom);
+
+            var userChatRoom = new userChatRoom
+            {
+                userId = creatorId,
+                chatRoomId = newRoom.chatRoomId,
+                joinedAt = DateTime.Now
+            };
+
+            _context.UserChatRooms.Add(userChatRoom);
+
+            _context.SaveChanges();
+
+            return newRoom;
+        }
+     
+        public chatMessage SendMessage(int senderId, int chatRoomId, string message)
+        {
+            var newMessage = new chatMessage
+            {
+                senderId = senderId,
+                chatRoomId = chatRoomId,
+                message = message,
+                SendAt = DateTime.Now
+            };
+            _context.ChatMessages.Add(newMessage);
+            _context.SaveChanges();
+
+            return newMessage;
+        }
+
+        public void AddUserToChatRoom(int userId, int roomId)
+        {
+            var userChatRoom = new userChatRoom
+            {
+                userId = userId,
+                chatRoomId = roomId,
+                joinedAt = DateTime.Now
+            };
+
+            _context.UserChatRooms.Add(userChatRoom);
+            _context.SaveChanges();
+        }
     }
 }
