695ba62918d5d8b1ef944072b01e10232d6533d8
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:37:12 +0530

    Create AddUsersRequest.cs

---
+++ Requests/AddUsersRequest.cs
@@ -0,0 +1,9 @@
+namespace backendChatApplcation.Requests
+{
+    public class AddUsersRequest
+    {
+        public int userId {  get; set; }
+
+        public int chatRoomId { get; set; }
+    }
+}
