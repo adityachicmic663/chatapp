4481ace63cd4f52c2ed21d02412f2cb13505e790
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:30:15 +0530

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,5 +7,131 @@ namespace backendChatApplcation.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+     private readonly IchatServices _chatService;
+ public ChatController(IchatServices chatService)
+ {
+     _chatService = chatService;
+ }
+ [HttpGet("GetRoomForUsers/{userId}")]
+ public ActionResult<IEnumerable<chatRoomModel>> GetRoomForUsers(int userId)
+ {
+     try
+     {
+         var chatRooms = _chatService.GetChatRoomsForUser(userId);
+         return Ok(new ResponseModel
+         {
+             statusCode = 200,
+             message = "get all your chat rooms here",
+             data = chatRooms,
+             isSuccess = true
+         });
+     } catch (Exception ex)
+     {
+         return StatusCode(500, new ResponseModel
+         {
+             statusCode = 500,
+             message = ex.InnerException?.Message ?? ex.Message,
+             data = "No data",
+             isSuccess = false
+         });
+     }
+ }
+
+ [HttpPost("CreateChatRoom")]
+ public ActionResult<chatRoomModel> CreateChatRoom(ChatRoomRequest request)
+ {
+     try
+     {
+         var newRoom = _chatService.CreateChatRoom(request.RoomName, request.CreatorId);
+         if (newRoom == null){
+             return BadRequest(new ResponseModel
+             {
+                 statusCode = 404,
+                 message = "no room created",
+                 data = "no data",
+                 isSuccess = false
+
+             });
+         }
+         return Ok(new ResponseModel
+         {
+             statusCode=200,
+             message="room created successfully",
+             data= newRoom,
+             isSuccess=true
+         });
+     }catch(Exception ex)
+     {
+         return StatusCode(500, new ResponseModel
+         {
+             statusCode = 500,
+             message = ex.InnerException?.Message ?? ex.Message,
+             data = "No data",
+             isSuccess = false
+         });
+     }
+    
+ }
+ [HttpPost("sendMessage")]
+ public ActionResult<chatMessage> SendMessage(sendMessageRequest request)
+ {
+     try
+     {
+         var newMessage = _chatService.SendMessage(request.senderId, request.chatRoomId, request.message);
+         if (newMessage == null)
+         {
+             return BadRequest(new ResponseModel
+             {
+                 statusCode=400,
+                 message="unable to send message",
+                 data="no data",
+                 isSuccess=false
+
+             });
+         }
+         return Ok(new ResponseModel
+         {
+             statusCode = 200,
+             message = "message is sent successfully",
+             data = newMessage,
+             isSuccess = true
+         });
+     }catch(Exception ex)
+     {
+         return StatusCode(500, new ResponseModel
+         {
+             statusCode = 500,
+             message = ex.InnerException?.Message ?? ex.Message,
+             data = "no data",
+             isSuccess = true
+         });
+     }
+ }
+ [HttpPut("AddUsers")]
+
+ public ActionResult AddUserToChatRoom(AddUsersRequest request)
+ {
+     try
+     {
+         _chatService.AddUserToChatRoom(request.userId, request.chatRoomId)
+         return Ok(new ResponseModel
+         {
+             statusCode = 200,
+             message = "status updated successfully",
+             data = "non data",
+             isSuccess = true
+         });
+     }
+     catch (Exception ex)
+     {
+         return StatusCode(500, new ResponseModel
+         {
+             statusCode = 500,
+             message = ex.Message,
+             data = "no data",
+             isSuccess = false,
+         });
+     }
+ }
     }
 }
