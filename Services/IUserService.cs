a79858f4594cb764b57ba864b28aa8b9db2c621d
adityachicmic663 <adiyta.bisht@chicmic.co.in>
2024-06-27 19:41:06 +0530

    Update IUserService.cs

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,6 +1,15 @@
-﻿namespace backendChatApplcation.Services
+using backendChatApplication;
+using backendChatApplication.Models;
+
+namespace backendChatApplcation.Services
 {
-    public interface IUserService
+    public interface IUserServices
     {
+
+        List<UserModel> SearchUser(string searchkey);
+
+        void UpdateUserStatus(int userId, bool isOnline);
+        
     }
 }
+
